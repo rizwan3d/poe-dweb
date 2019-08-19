bfc6fdcbc789a4bc8b23d421b53348e488d6d598
Muhammad Rizwan <allgame45@hotmail.com>
2019-08-20 00:35:40 +0500

    Signed-off-by: Muhammad Rizwan <allgame45@hotmail.com>

--- api/v1-c/index.php
+++ api/v1-c/index.php
@@ -3,5 +3,6 @@
     $command = "curl --user multichainrpc:F1oHsbZnRtbxqSr5T5NVXYbfFUMmkT73DV7vYTE7Fi93 --data '";
     $command = $command . '{"method":"getinfo", "params":[], "id":"4"}';
     $command = $command . "' -H 'content-type: text/plain;' localhost:2686/";
+    echo $command;
     echo shell_exec($command);
 ?>
