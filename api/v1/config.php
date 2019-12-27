f88027471348c96e094abfd3fa52e641304ab2a8
Muhammad Rizwan <allgame45@hotmail.com>
2019-12-27 22:46:23 +0500

    ok

--- api/v1/config.php
+++ api/v1/config.php
@@ -10,12 +10,28 @@
 //  'address' => "1UKJvibJC5fhdRJR3DNL5EYG7nAQe9xLhhCTyM"
 //  );
 
+$tt = 0;
 
+
+if($tt == 1){
 return array(
      'rk_user' => 'multichainrpc',
-     'rk_pass' => 'F1oHsbZnRtbxqSr5T5NVXYbfFUMmkT73DV7vYTE7Fi93',
+     'rk_pass' => 'HqbqAf85sgDM9aNVdg6fEEXds5d2vYTbNFW4M7Ri7uPr',
      "rk_host" => 'localhost',
-     "rk_port" => '2686',
+     "rk_port" => '4790',
      "chain_name" => 'chain1',
-     'address' => "179Qh3QXhZufGpbLt3Uf54e7hrW4cZLvAEPrFy"
+     'address' => "1FUZyqq1JWHKxZAyoPeQmhPgK5cyTD7LzNZunE"
      );
+}
+
+else 
+{     
+return array(
+     'rk_user' => 'multichainrpc',
+     'rk_pass' => '8smWyPTC4hakqTP6zWH6Z3VJQqBf7benXTtyYbD4eSWL',
+     "rk_host" => 'localhost',
+     "rk_port" => '6807',
+     "chain_name" => 'chain1',
+     'address' => "13LFV8mUFYVTLyTspAzAYTsYom6hBVfniSGS7i"
+     );
+}
